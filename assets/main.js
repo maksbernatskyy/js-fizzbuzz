2c47d7ab503983d98ef3f5a1422ca84363163bd1
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-07 13:46:56 +0200

    add human code

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,9 @@ TOOLS:
 - if / else
 */
 
+/* Stampo i numeri da 1 a 100 */
+
+// Apro un ciclo 'for'
+// Stampo 'Fizz' per i numeri che sono multipli di 3
+// Stampo 'Buzz' per i numeri che sono multipli di 5
+// Stampo 'FizzBuzz' per i numeri che sono multipli sia di 3 che di 5
