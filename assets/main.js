ce1467f21ec7e7f97946c23dcfd1625db55eed0c
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-07 14:36:39 +0200

    add if into for

--- assets/main.js
+++ assets/main.js
@@ -6,12 +6,35 @@ TOOLS:
 - if / else
 */
 
+/* Inizializzo le stringhe */
+
+// Inizializzo 'Fizz'
+const fizz = 'Fizz'
+
+// Inizializzo 'Buzz'
+const buzz = 'Buzz'
+
+// Inizializzo 'FizzBuzz'
+const fizzBuzz = 'FizzBuzz'
+
 /* Stampo i numeri da 1 a 100 */
 
 // Apro un ciclo 'for'
 for (let i = 1; i <= 100; i++){
     console.log(i)
+
+    if (i % 3 === 0 && i % 5 === 0){
+        // Stampo 'FizzBuzz' per i numeri che sono multipli sia di 3 che di 5
+        console.log(fizzBuzz)
+
+    } else if (i % 3 === 0){
+        // Stampo 'Fizz' per i numeri che sono multipli di 3
+        console.log(fizz)
+
+    } else if (i % 5 === 0){
+        // Stampo 'Buzz' per i numeri che sono multipli di 5
+        console.log(buzz)
+    }
 }
-// Stampo 'Fizz' per i numeri che sono multipli di 3
-// Stampo 'Buzz' per i numeri che sono multipli di 5
-// Stampo 'FizzBuzz' per i numeri che sono multipli sia di 3 che di 5
+
+
