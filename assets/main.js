9b053a8bee57967a61fcb31a767a58f7008b2306
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-07 13:49:27 +0200

    add 'for' for stamp numbers from 1 to 100

--- assets/main.js
+++ assets/main.js
@@ -9,6 +9,9 @@ TOOLS:
 /* Stampo i numeri da 1 a 100 */
 
 // Apro un ciclo 'for'
+for (let i = 1; i <= 100; i++){
+    console.log(i)
+}
 // Stampo 'Fizz' per i numeri che sono multipli di 3
 // Stampo 'Buzz' per i numeri che sono multipli di 5
 // Stampo 'FizzBuzz' per i numeri che sono multipli sia di 3 che di 5
